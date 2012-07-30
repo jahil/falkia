da8145adef2757e534e87337d2d63362d0b457f3
ghafil <named.conf@gmail.com>
2012-07-30 10:44:00 +0400

    added smtp mailer block

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,9 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+config.action_mailer.smtp_settings = {
+:enable_starttls_auto => true,
+:address=>"localhost",
+:port=>25
+}
+
